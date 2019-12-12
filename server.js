9a11cb1b0f98eb6ce40bd4c1bdffbaeaa14b93ee
mayckcoelho <mayckcoelho0@gmail.com>
2019-12-12 00:47:54 -0300

    correção 1

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.use(function (req,res,next) { 
-  req.url = req.url.replace(/[/]+/g, '/'); next(); 
 });
 
 app.listen(21178, () => console.log(`Servidor subiu com sucesso na porta 21178`))
