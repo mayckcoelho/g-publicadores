8c78334007c6b5014757a74617e3b936a72fd3ad
mayckcoelho <mayckcoelho0@gmail.com>
2019-12-12 11:42:58 -0300

    commit build e servidor

---
+++ build/precache-manifest.ce4f23d535d680712f2499245b4b08b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f4e566636b1f93b2a00f8a5025a146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "173ddad761d2e1cab450",
+    "url": "/static/css/2.a8353e22.chunk.css"
+  },
+  {
+    "revision": "173ddad761d2e1cab450",
+    "url": "/static/js/2.1510c575.chunk.js"
+  },
+  {
+    "revision": "4b516ad45992ecd4a763",
+    "url": "/static/js/main.e44092fd.chunk.js"
+  },
+  {
+    "revision": "8e8617a3751fcbdc7d0d",
+    "url": "/static/js/runtime-main.8ff8c096.js"
+  },
+  {
+    "revision": "e654bf1a5c6245c9575a20ac7f8d6686",
+    "url": "/static/media/bg.e654bf1a.jpg"
+  },
+  {
+    "revision": "d3cac432ceff4ebf7890b14817509507",
+    "url": "/static/media/logo.d3cac432.png"
+  }
+]);
