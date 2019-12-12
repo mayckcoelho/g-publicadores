44a998563428d2233bf9b5a2334ccfed17b558f0
mayckcoelho <mayckcoelho0@gmail.com>
2019-12-12 00:36:56 -0300

    desespero

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.use(function (req,res,next) { 
+  req.url = req.url.replace(/[/]+/g, '/'); next(); 
+});
+
 app.listen(21178, () => console.log(`Servidor subiu com sucesso na porta 21178`))
