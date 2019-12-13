786f5e38f00037de05155153f5302432f0f0fe14
mayckcoelho <mayckcoelho0@gmail.com>
2019-12-13 10:29:17 -0300

    alteração do base_url

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export default {
-    BASE_URL: 'http://publicadores.tjcampestre.com.br/api',
+    BASE_URL: 'https://tjcampestre.com.br:21147',
     USER_TOKEN: 'user_token',
     USER_DATA: 'user_data'
 }
