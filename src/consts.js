9a50f02241ba5f374c23356bb1ec2f3b3cb0f6a6
Mayck Coelho <mayckcoelho@MacBook-Air-de-Mayck.local>
2020-08-17 20:22:07 -0300

    Atualização da base url

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export default {
-    BASE_URL: 'https://tjcampestre.com.br:21147',
+    BASE_URL: 'http://tjcampestre.com.br:21147',
     USER_TOKEN: 'user_token',
     USER_DATA: 'user_data',
     ALLOWED_EMAILS: [
